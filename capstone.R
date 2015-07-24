afc86b899a591366e4e706e790bb838aef38734f
John McKinzie <john@johnmckinzie.com>
2015-07-23 23:01:49 -0400

    Added tokenization to n-grams.

--- capstone.R
+++ capstone.R
@@ -11,30 +11,81 @@
 # 
 # close(con) ## It's important to close the connection when you are done
 
+library(tm)
+library(RWeka)
+# library(parallel)
+options(mc.cores=1)
+
+kBadWordUrl <- "https://raw.githubusercontent.com/shutterstock/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/master/en"
+kBadWordDest <- "bad_words.txt" # https://github.com/shutterstock/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words
 kDataDir <- "data"
+kDataSet <- "sample"
+kLang <- "en_US"
+kCourpusDir <- file.path(kDataDir, kDataSet, kLang)
+
+
+GetBadWords <- function() {
+  download.file(kBadWordUrl, kBadWordDest)
+}
+
+GetCorpusFiles <- function() {
+  list.files(kCourpusDir)
+}
+
+files.corpus <- GetCorpusFiles()
+print(files.corpus)
+
+GetFileStats <- function() {
+  files.corpus.stats <- data.frame(Date=as.Date(character()),
+  File=character(), 
+  User=character(), 
+  stringsAsFactors=FALSE) 
+  files.corpus.sizes <- lapply(files.corpus, function(file) {
+    path <- paste(kCourpusDir, file, sep = "/")
+    out <- system(paste("wc -lw", path), intern = TRUE)
+    out <- strsplit(out," ")
+    print(out)
+  })
+}
+
+GetFileStats()
+
+# remove
+CleanAndTransformCorpus <- function(corpus) {
+  corpus <- tm_map(corpus, removePunctuation)
+  corpus <- tm_map(corpus, removeNumbers)
+  corpus <- tm_map(corpus, stripWhitespace)
+  corpus <- tm_map(corpus, content_transformer(tolower))
+  corpus <- tm_map(corpus, removeWords, c(stopwords("english"), readLines(kBadWordDest)), lazy = TRUE)
+  corpus <- tm_map(corpus, stemDocument, language = "english", lazy = TRUE)
+  corpus <- tm_map(corpus, PlainTextDocument)
+  return (corpus)
+}
+
 
 library(tm)
-a <- Corpus(DirSource(file.path(kDataDir, "en_US_head")), readerControl = list(language="en-US"))
-a <- tm_map(a, removeNumbers)
-a <- tm_map(a, removePunctuation)
-a <- tm_map(a , stripWhitespace)
-a <- tm_map(a, content_transformer(tolower))
-a <- tm_map(a, removeWords, stopwords("english"))
-a <- tm_map(a, stemDocument, language = "english") # I also got it to work with stemming, but it takes so long...
-# remove words longer than 28, https://en.wikipedia.org/wiki/Longest_word_in_English
-# remove bad words https://github.com/shutterstock/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words
-adtm <-DocumentTermMatrix(a) 
-adtm <- removeSparseTerms(adtm, 0.75)
+c <- Corpus(DirSource(kCourpusDir), readerControl = list(language = "en-US"))
+c <- CleanAndTransformCorpus(c)
 
-inspect(adtm) 
+# adtm <-DocumentTermMatrix(a) 
+# adtm <- removeSparseTerms(adtm, 0.75)
+# 
+# inspect(adtm) 
+# 
+# # print(findFreqTerms(adtm, lowfreq=10)) # find terms with a frequency higher than 10
+# print(findAssocs(adtm, "usa",.5)) # just looking for some associations  
+# # findAssocs(adtm, "china",.5)
 
-# print(findFreqTerms(adtm, lowfreq=10)) # find terms with a frequency higher than 10
-print(findAssocs(adtm, "usa",.5)) # just looking for some associations  
-# findAssocs(adtm, "china",.5)
+# tokenizers
+TokenizerUnigram <- function(x) NGramTokenizer(x, Weka_control(min = 1, max = 1))
+TokenizerBigram <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
+TokenizerTrigram <- function(x) NGramTokenizer(x, Weka_control(min = 3, max = 3))
 
-# Trigrams
-# require(RWeka)
-# TrigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 3, max = 3))
-# tdm <- TermDocumentMatrix(a, control = list(tokenize = TrigramTokenizer))
+# generate TDMs
+tdm.unigram <- TermDocumentMatrix(c, control = list(tokenize = TokenizerUnigram))
+tdm.bigram <- TermDocumentMatrix(c, control = list(tokenize = TokenizerBigram))
+tdm.trigram <- TermDocumentMatrix(c, control = list(tokenize = TokenizerTrigram))
+
+print(tdm.trigram[1:10, ])
 # tdm <- removeSparseTerms(tdm, 0.75)
-# inspect(tdm[1:5,1:5])
+inspect(tdm.trigram[1:10, ])
